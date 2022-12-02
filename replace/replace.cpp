8c50653a21a960489d5beb9f7630a76c405f92c7
labobabo <maksim.ko.85@mail.ru>
2022-12-02 18:00:23 +0300

    replace_into_branch

---
+++ replace/replace.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string sent;
+	cout << "write you message"<<endl;
+
+	getline(cin, sent);
+
+	for (int i = 0,j=sent.length(); i<(sent.length() / 2);  i++,j--)// half of string and replace
+	{
+		swap(sent[i], sent[j]);
+	}
+	cout << sent;
+}
